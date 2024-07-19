32e725331dd4089197f1c69dcffb5574fcf93f08
Madhuri Tugave <[dalvemadhuri974@gmail.com]>
2024-07-19 13:19:12 +0530

    code is structcred and comments add

--- src/components/home/homeUp.jsx
+++ src/components/home/homeUp.jsx
@@ -1,20 +1,23 @@
-import '../../style/home/home-1.css';
-import '../../style/home/home-4.css';
-import { Col, Container, Row } from "react-bootstrap"
-import { TopImages } from "./Tbackground"
+import { Col, Container, Row } from "react-bootstrap";
+import { TopImages } from "./Tbackground";
+import "../../style/home/home-1.css";
+import "../../style/home/home-4.css";
 
-export const HomeUp = () =>{
-    return(
-        <Container fluid className="top-home">
-        <Row className="justify-content-center align-items-center h-20">
-          <Col xs={12} md={8} lg={6}>
-            <h1 className="tagline text-center">Welcome to To-Let Globe</h1>
-            <h6 className="headline text-center">
-              NO BROKERAGE ON PGS | FLATS | HOUSES | OFFICES
-            </h6>
-              <TopImages />
-          </Col>
-        </Row>
-      </Container>
-    )
-}
+export const HomeUp = () => {
+  return (
+    <Container fluid className="top-home">
+      <Row className="justify-content-center align-items-center h-20">
+        <Col xs={12} md={8} lg={6}>
+          {/* Heading */}
+          <h1 className="tagline text-center">Welcome to To-Let Globe</h1>
+          {/* Sub Headings */}
+          <h6 className="headline text-center">
+            NO BROKERAGE ON PGS | FLATS | HOUSES | OFFICES
+          </h6>
+          {/* TopImages component has all tha Top Images */}
+          <TopImages />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
